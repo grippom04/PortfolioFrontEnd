61beec34cc270e58b1472423842d3b62cbad68f3
grippom04 <103483194+grippom04@users.noreply.github.com>
2023-03-24 04:15:00 -0300

    header superior mejorado con bootstrap - falta loguin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,23 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { SupHeaderComponent } from './components/sup-header/sup-header.component';
 import { APlogoComponent } from './components/aplogo/aplogo.component';
 import { SocialLinksComponent } from './components/social-links/social-links.component';
 import { LogButtonComponent } from './components/log-button/log-button.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     SupHeaderComponent,
     APlogoComponent,
     SocialLinksComponent,
     LogButtonComponent,
+    BannerComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule
